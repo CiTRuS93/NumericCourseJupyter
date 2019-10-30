f57eedb76ce28703741ce6fe5c35d8b7e45a6df6
hadar <hadarradah@gmail.com>
2019-10-30 18:27:57 +0200

    finished interpolation part

--- interpolation/interpolation_method.py
+++ interpolation/interpolation_method.py
@@ -16,4 +16,27 @@ def  interpolation(x,xm ,ym):
     y = np.dot(ym ,lagrpoly)
     return y
 
+#newton interpolation
+
+def NDD(x,y):
+    n = len(x)
+    #Construct table and load xy pairs in first columns
+    A = zeros((n,n+1))
+    A[:,0]= x[:]
+    A[:,1]= y[:]
+    #Fill in Divided differences
+    for j in range(2,n+1):
+        for i in range(j-1,n):
+            A[i,j] = (A[i,j-1]-A[i-1,j-1]) / (A[i,0]-A[i-j+1,0])
+    #Copy diagonal elements into array for returning
+    p = zeros(n)
+    for k in range(0,n):
+        p[k] = A[k,k+1]
+        return p
+def poly(t,x,p):
+    n = len(x)
+    out = p[n-1]
+    for i in range(n-2,-1,-1):
+        out = out*(t-x[i]) + p[i]
+        return out
 
